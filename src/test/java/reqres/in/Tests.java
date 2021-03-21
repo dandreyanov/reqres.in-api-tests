5080855fd190af2e32674e4c627713839cb6fda6
Denis Andreyanov <andreyanov.denis92@gmail.com>
2021-03-21 14:20:17 +0300

    added tests

---
+++ src/test/java/reqres/in/Tests.java
@@ -0,0 +1,105 @@
+package reqres.in;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static utils.FileUtils.readStringFromFile;
+
+public class Tests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.filters(new AllureRestAssured());
+        RestAssured.baseURI = "https://reqres.in/api/";
+    }
+
+    @Test
+    void createUsersTest() {
+        given()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
+                .when()
+                .post("users")
+                .then()
+                .statusCode(201)
+                .log().body()
+                .body("name",
+                        is("morpheus"))
+                .body("job",
+                        is("leader"))
+                .body("id",
+                        is(notNullValue()))
+                .body("createdAt",
+                        is(notNullValue()))
+        ;
+
+    }
+
+    @Test
+    void updateUsersTest() {
+        given()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"zion resident\" }")
+                .when()
+                .put("users/2")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .body("name",
+                        is("morpheus"))
+                .body("job",
+                        is("zion resident"))
+                .body("updatedAt",
+                        is(notNullValue()))
+        ;
+
+    }
+
+    @Test
+    void deleteUsersTest() {
+        given()
+                .when()
+                .delete("users/2")
+                .then()
+                .statusCode(204)
+        ;
+    }
+
+    @Test
+    void notFoundTest() {
+        given()
+                .when()
+                .get("unknown/23")
+                .then()
+                .statusCode(404)
+        ;
+    }
+
+    @Test
+    void getListUsers() {
+        String listUser = readStringFromFile("./src/test/resources/ListUsersData.txt");
+        given()
+                .when()
+                .get("users?page=2")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .body("",
+                        is(listUser))
+        ;
+    }
+
+
+
+}
